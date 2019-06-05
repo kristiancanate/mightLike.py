ce7ccc312f0da8065d327820e9b2dd2e3df4c6a4
Guest User <Guest@AD25TF04NGF1J-69.local>
2019-06-05 12:38:35 -0400

    set up structure of program

--- mightLike.py
+++ mightLike.py
@@ -1,1 +1,10 @@
 #mightLike by Kristian, Darius, Shandrika, Mariana, and Eric
+
+print("""In this program, you will first be asked for a song and
+different elements about it. BLAHBLAHBLAHBLAH.
+When you're ready, hit 'return' to start.""")
+input()
+
+songT = input("""What is the name of the song?
+Song Name: """)
+print("")

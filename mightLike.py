2f8de04e734caf5078ec6c273c57741ce829a221
Guest User <Guest@AD25TF04NGF1J-40.local>
2019-06-05 12:58:54 -0400

    added variables and questions for user song

--- mightLike.py
+++ mightLike.py
@@ -5,6 +5,54 @@ different elements about it. BLAHBLAHBLAHBLAH.
 When you're ready, hit 'return' to start.""")
 input()
 
-songT = input("""What is the name of the song?
+uTi = input("""What is the name of the song?
 Song Name: """)
+print("")
+uCr = input(""" what is the creator of this song?
+creator Name: """)
+print("")
+uPer = input(""" who is the performer?
+Performer Name: """)
+print("")
+uLang = input(""" what is the language of this song?
+Language: """)
+print ("")
+uDate = input(""" what date was this song recorded?
+Date: """)
+print("")
+uGen = input(""" what the genre of this song?
+Genre: """)
+print("")
+uSubG = input(""" what the sub-genre of this song?
+Sub-genre: """)
+print("")
+uTop = input(""" what the topic of the song?
+Topic: """)
+print("")
+uMood = input(""" what the mood?
+Mood: """)
+print("")
+uDI = input(""" what the dominant instrument?
+instrument: """)
+print("")
+uISk = input(""" Rate the intsrum
+""")
+print("")
+uLy = input(""" Rate the
+""")
+print("")
+uVo = input(""" Rate the
+""")
+print("")
+uMel = input(""" Rate the
+""")
+print("")
+uRhy = input(""" Rate the
+""")
+print("")
+uDDR = input(""" Rate the
+""")
+print("")
+uTpo = input(""" Rate the
+""")
 print("")

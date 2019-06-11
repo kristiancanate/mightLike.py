ebc8ab74adc1335e749e0f5a6ad74dd964f73959
csPython <cspython@AD25TF04NGF1J-78.local>
2019-06-11 11:52:24 -0400

    displays song library

--- algorithumML.py
+++ algorithumML.py
@@ -1,4 +1,21 @@
-if SongLGen == uGen:
-    print()
-else:
-    print()
+# Algorithum for SL
+
+def formatMusic(fileName):
+    songLib = open(fileName,"r")
+    songLib_data = []
+    for line in songLib:
+        number_songs = line.split("\t")
+        songs = [n for n in number_songs]
+        songLib_data.append(songs)
+    for song in songLib_data:
+        song[16] = (song[16][:1])
+    songLib_data_tuple = tuple(songLib_data)
+    return songLib_data_tuple
+
+songList = formatMusic("songLib.tsv")
+print(songList)
+
+#if SongLGen == uGen:
+    #print()
+#else:
+ #   print()

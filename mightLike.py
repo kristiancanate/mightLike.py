61ca06f248ce923ba9a13ec7c9a3dfd0eafb7ad9
Kristian <kristiankxc@gmail.com>
2019-06-13 19:03:13 -0400

    gets rid of function

--- mightLike.py
+++ mightLike.py
@@ -7,47 +7,41 @@ different elements about it. BLAHBLAHBLAHBLAH.
 When you're ready, hit 'return' to start.""")
 input()
 
-def run_quest (var, que):
-    var = input(que)
-    print("")
-    global inp
-    inp = var
-
 #user input
-run_quest(uTi, uTiQ)
-uTi = inp
-run_quest(uCr, uCrQ)
-uCr = inp
-run_quest(uPer, uPerQ)
-uPer = inp
-run_quest(uLang, uLangQ)
-uLang = inp
-run_quest(uDate, uDateQ)
-uDate = inp
-run_quest(uGen, uGenQ)
-uGen = inp
-run_quest(uSubG, uSubGQ)
-uSubG = inp
-run_quest(uTop, uTopQ)
-uTop = inp
-run_quest(uMood, uMoodQ)
-uMood = inp
-run_quest(uDI, uDIQ)
-uDI = inp
-run_quest(uISk, uISkQ)
-uISk = inp
-run_quest(uLy, uLyQ)
-uLy = inp
-run_quest(uVo, uVoQ)
-uVo = inp
-run_quest(uMel, uMelQ)
-uMel = inp
-run_quest(uRhy, uRhyQ)
-uRhy = inp
-run_quest(uDDR, uDDRQ)
-uDDR = inp
-run_quest(uTpo, uTpoQ)
-uTpo = inp
+uTi = input(uTiQ)
+print("")
+uCr = input(uCrQ)
+print("")
+uPer = input(uPerQ)
+print("")
+uLang = input(uLangQ)
+print("")
+uDate = input(uDateQ)
+print("")
+uGen = input(uGenQ)
+print("")
+uSubG = input(uSubGQ)
+print("")
+uTop = input(uTopQ)
+print("")
+uMood = input(uMoodQ)
+print("")
+uDI = input(uDIQ)
+print("")
+uISk = input(uISkQ)
+print("")
+uLy = input(uLyQ)
+print("")
+uVo = input(uVoQ)
+print("")
+uMel = input(uMelQ)
+print("")
+uRhy = input(uRhyQ)
+print("")
+uDDR = input(uDDRQ)
+print("")
+uTpo = input(uTpoQ)
+print("")
 
 print(uTi, uCr, uPer, uLang, uDate, uGen, uSubG, uTop, uMood, uDI, uISk,
       uLy, uVo, uMel, uRhy, uDDR, uTpo)
